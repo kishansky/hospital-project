157174dc9c8f33b7e0d8c6257bf3f00e558ba766
Kishan Kumar <110089579+kishansky@users.noreply.github.com>
2023-08-23 08:20:29 +0530

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,9 +1,9 @@
 <?php
 
 
-  $hostname ="https://adarshcare.com/";
+  $hostname ="localhost/";
 
 
- $conn = mysqli_connect("localhost","u533246364_aman","Kishan@2324","u533246364_hospital") or die("connection failed :". mysqli_connect_error());
+ $conn = mysqli_connect("localhost","root","","u533246364_hospital") or die("connection failed :". mysqli_connect_error());
 
 ?>
